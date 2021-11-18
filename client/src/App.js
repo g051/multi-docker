f4e64041dec6d3dae5a83785d45bf3956b0808c6
Gosi <support@guoyd.com>
2021-11-18 23:31:26 +0800

    change header

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
-import OtherPage from './OtherPage';
-import Fib from './Fib';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Welcome to React</h1>
+          <h1>Fib Calculator</h1>
           <Link to="/">Home</Link>
           <Link to="otherpage">Other Page</Link>
         </header>
